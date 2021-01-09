8ed4d353b17a40fea7eeaf3bee55c15d8fd6a26a
samir692 <samir.huseynzade.966@gmail.com>
2021-01-09 18:24:35 +0100

    starter springboot project is added

---
+++ src/main/java/com/guru/trade/GuruTradeApplication.java
@@ -0,0 +1,13 @@
+package com.guru.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guru/trade/GuruTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
