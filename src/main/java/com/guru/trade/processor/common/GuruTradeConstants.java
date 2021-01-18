2a279ef44dd3b2c4822c0b236826112c415a5626
samir692 <samir.huseynzade.966@gmail.com>
2021-01-18 09:14:27 +0100

    Constants added for Buy thresholds

---
+++ src/main/java/com/guru/trade/processor/common/GuruTradeConstants.java
@@ -0,0 +1,11 @@
+package com.guru.trade.processor.common;
+
+public class GuruTradeConstants {
+
+
+  public static class BuySellThresholds {
+    private static final String BUY_UPWARD_TREND_THRESHOLD = "UPWARD_TREND_THRESHOLD";
+    private static final String BUY_DOWNWARD_TREND_THRESHOLD = "DOWNWARD_TREND_THRESHOLD";
+
+  }
+}
